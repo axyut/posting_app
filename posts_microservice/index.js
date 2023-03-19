b419a0a381978cdfada1d5533ff2c3ac279f70e9
Achyut <aoogleak@gmail.com>
2023-03-19 15:36:58 +0545

    1.0 initaial posting microservice

---
+++ posts_microservice/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const app = express();
+
+// bodyparser
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+	res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+	const id = randomBytes(4).toString("hex");
+	const { title } = req.body;
+	console.log(title);
+	posts[id] = {
+		id,
+		title,
+	};
+
+	res.status(201).send(posts[id]);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Running at http://localhost:${PORT}`);
+});
